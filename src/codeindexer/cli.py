184b195beee1ba9570193c8709d96eafe946dd0a
Kaushal Kishore <kshr4kshl@gmail.com>
2026-05-01 19:09:38 +0530

    func definition basic

--- src/codeindexer/cli.py
+++ src/codeindexer/cli.py
@@ -11,20 +11,28 @@ def get_argparser() -> argparse.ArgumentParser:
     # Add arguments here as needed
     return parser
 
+def walk_tree_cpp(node):
+    if node.type == "function_definition":
+        node_name = node.child_by_field_name("declarator")
+        print(f"Found function: {node_name.text.decode('utf-8')}")
+    
+    for child in node.children:
+        walk_tree_cpp(child)
+
+def parse_cpp_file(file_path: str) -> None:
+    with open(file_path, 'rb') as f:
+        code_bytes = f.read()
+        
+    CPP_LANGUAGE = Language(tscpp.language())
+    parser = Parser(CPP_LANGUAGE)
+    tree = parser.parse(code_bytes)
+    walk_tree_cpp(tree.root_node)
+
 def main() -> None:
     parser = get_argparser()
     args = parser.parse_args()
     if args.cpp:
-        print(f"Indexing C++ file: {args.cpp}")
-        with open(args.cpp, 'rb') as f:
-            code_bytes = f.read()
-            
-        CPP_LANGUAGE = Language(tscpp.language())
-        parser = Parser(CPP_LANGUAGE)
-        tree = parser.parse(code_bytes)
-        print(tree)
-    print("Hello from codeindexer cli!")
-
+        parse_cpp_file(args.cpp)
 
 if __name__ == "__main__":
     main()
--- test_cpp/test.cpp
+++ test_cpp/test.cpp
@@ -1,3 +1,7 @@
+int sum(int a, int b) {
+    return a + b;
+}
+
 int main() {
     return 0;
 }
